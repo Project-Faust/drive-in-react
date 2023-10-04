a090c77ed0a78ef525510bade87e0fdba3a3745e
Project-Faust <pentecost.cody.lane@gmail.com>
2023-10-03 20:27:51 -0400

    updated rules/menu to display as jpg instead of pdf

--- src/pages/Menu.js
+++ src/pages/Menu.js
@@ -1,13 +1,20 @@
 import MenuPDF from "../assets/drive-in-menu.pdf"
+import menuJPG from "../assets/drive-in-menu.jpg"
 
 export default function Menu() {
     return (
-        <section className="pdfSection p-3">
-            <div className="text-center">
+        <section className="pdfSection container-fluid">
+            <div className="row text-center py-3">
                 <h1>Our Concession Stand is Open!</h1>
             </div>
-            <div className="pdfContainer text-center">
-                <iframe className="pdfViewer border border-3 border-dark rounded" src={MenuPDF} title="Menu PDF" width="100%" height="100%"></iframe>
+            {/* <div className="pdfContainer text-center p-3">
+                <iframe className="pdfViewer border border-3 border-dark rounded mb-0" src={MenuPDF} title="Menu PDF" width="100%" height="100%"></iframe>
+            </div> */}
+            <div className="d-flex justify-content-center row mw-100 p-3">
+                <img
+                src={menuJPG}
+                alt="menu"
+                className="col-lg-6 col-md-12 border border-3 border-dark p-0"/>
             </div>
         </section>
     );
--- src/pages/Rules.js
+++ src/pages/Rules.js
@@ -1,13 +1,20 @@
 import rulesPDF from "../assets/drive-in-rules.pdf"
+import rulesJGP from "../assets/drive-in-rules.jpg"
 
 export default function Rules() {
     return (
-        <section className="pdfSection p-3">
-            <div className="text-center">
+        <section className="pdfSection container-fluid">
+            <div className="text-center p-3">
                 <h1>Here are our rules and regulations!</h1>
             </div>
-            <div className="pdfContainer text-center">
+            {/* <div className="pdfContainer text-center p-3">
                 <iframe className="pdfViewer border border-3 border-dark rounded" src={rulesPDF} title="Rules PDF" width="100%" height="100%"></iframe>
+            </div> */}
+            <div className="d-flex justify-content-center row mw-100 p-3">
+                <img
+                src={rulesJGP}
+                alt="rules"
+                className="col-lg-6 col-md-12 border border-3 border-dark p-0"/>
             </div>
         </section>
     );
