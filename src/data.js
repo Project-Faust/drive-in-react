70ca94cdb76da7669e69126e92328f0cb417d261
Lane Pentecost <pentecost.cody.lane@gmail.com>
2023-11-12 10:55:15 -0500

    updated for weekend of 11/17/2023

--- src/data.js
+++ src/data.js
@@ -9,14 +9,14 @@ const template = [
 
 export const nowPlaying = [
         {
-            title: "Smokey and the Bandit (1977)",
-            image: require("./assets/smokey-and-the-bandit-1977.jpg"),
-            imdb: "https://www.imdb.com/title/tt0076729/",
+            title: "Jurrasic Park (1993)",
+            image: require("./assets/jurassic-park-1993.jpg"),
+            imdb: "https://www.imdb.com/title/tt0107290/",
         },
         {
-            title: "American Graffiti (1973)",
-            image: require("./assets/american-graffiti-1973.jpg"),
-            imdb: "https://www.imdb.com/title/tt0069704/"
+            title: "Twister (1996)",
+            image: require("./assets/twister-1996.jpg"),
+            imdb: "https://www.imdb.com/title/tt0117998/"
         }
 ]
 
@@ -45,43 +45,4 @@ export const comingSoon = [
     //     image: require(""),
     //     imdb: "",
     // }
-]
-
-// eslint-disable-next line
-export const previousPlaying = [
-    {
-        title: "Paw Patrol: The Mighty Movie",
-        image: require("./assets/paw-patrol-the-mighty-movie-poster.jpg"),
-        imdb: "https://www.imdb.com/title/tt15837338/",
-    },
-    {
-        title: "Scream VI",
-        image: require("./assets/scream-vi-poster.jpg"),
-        imdb: "https://www.imdb.com/title/tt17663992/",
-    },
-    {
-        title: "Transformers: Rise of the Beasts",
-        image: require("./assets/transformers-rise-of-the-beasts-poster.jpg"),
-        imdb: "https://www.imdb.com/title/tt5090568/",
-    },
-    {
-        title: "Gran Turismo",
-        image: require("./assets/gran-turismo-poster.jpg"),
-        imdb: "https://www.imdb.com/title/tt4495098/",
-    },
-    {
-        title: "The Equalizer 3",
-        image: require("./assets/equalizer-3-poster.jpg"),
-        imdb: "https://www.imdb.com/title/tt17024450/",
-    },
-    {
-        title: "Blue Beetle",
-        image: require("./assets/blue-beetle-poster.jpg"),
-        imdb: "https://www.imdb.com/title/tt9362930/",
-    },
-    {
-        title: "Barbie",
-        image: require("./assets/barbie-poster.jpg"),
-        imdb: "https://www.imdb.com/title/tt1517268/",
-    }
 ]
