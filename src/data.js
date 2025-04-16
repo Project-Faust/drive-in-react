a0b600b47326b277814b04eb5b814532c8af7925
Project-Faust <pentecost.cody.lane@gmail.com>
2025-04-16 12:18:55 -0400

    updated for upcoming weekend; updated img file path

--- src/data.js
+++ src/data.js
@@ -10,12 +10,12 @@ const template = [
 export const nowPlaying = [
     {
         title: "King of Kings",
-        image: require("../src/assets/king-of-kings-2025.webp"),
+        image: require("../src/assets/movie-posters/king-of-kings-2025.webp"),
         imdb: "https://www.imdb.com/title/tt7967302/",
     },
     {
         title: "A Minecraft Movie",
-        image: require("./assets/a-minecraft-movie-2025.webp"),
+        image: require("./assets/movie-posters/a-minecraft-movie-2025.webp"),
         imdb: "https://www.imdb.com/title/tt3566834/",
     },
 ]
@@ -36,13 +36,13 @@ export const toBeDetermined = [
 // eslint-disable-next-line
 export const comingSoon = [
     {
-        title: "To Be Determined",
-        image: require("./assets/to-be-determined.jpg"),
-        imdb: "",
+        title: "Star Wars: Episode III - Revenge of the Sith",
+        image: require("./assets/movie-posters/star-wars-ep3.webp"),
+        imdb: "https://www.imdb.com/title/tt0121766/",
     },
     {
-        title: "To Be Determined",
-        image: require("../src/assets/to-be-determined.jpg"),
-        imdb: "",
+        title: "Spaceballs",
+        image: require("../src/assets/movie-posters/spaceballs.webp"),
+        imdb: "https://www.imdb.com/title/tt0094012/",
     }
 ]
