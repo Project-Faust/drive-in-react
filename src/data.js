7c5320745c98c62bbe8abb9a6cc8d431f2f31f79
Project-Faust <pentecost.cody.lane@gmail.com>
2025-05-19 16:22:08 -0400

    updated weekend 5/23

--- src/data.js
+++ src/data.js
@@ -9,13 +9,13 @@ const template = [
 
 export const nowPlaying = [
     {
-        title: "Twister",
-        image: require("./assets/movie-posters/twister-1996.webp"),
-        imdb: "https://www.imdb.com/title/tt0117998/",
+        title: "Lilo & Stitch",
+        image: require("./assets/movie-posters/lilo-and-stitch-2025.webp"),
+        imdb: "https://www.imdb.com/title/tt11655566/"
     }, {
-        title: "Twisters",
-        image: require("./assets/movie-posters/twisters-2024.webp"),
-        imdb: "https://www.imdb.com/title/tt12584954/",
+        title: "The Last Rodeo",
+        image: require("../src/assets/movie-posters/the-last-rodeo-2025.webp"),
+        imdb: "https://www.imdb.com/title/tt30908405/",
     },
 ]
 
