ae7726a3efe92a26486899db7a90b3f27c4604bf
Project-Faust <pentecost.cody.lane@gmail.com>
2025-06-02 15:07:45 -0400

    implemented ad carousel to homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -5,6 +5,7 @@ import ifYoureLooking from "../assets/if-youre-looking.webp";
 import logo from "../assets/logo.webp"
 import starryBG from "../assets/star-background.webp"
 import starryBGport from "../assets/star-background-portrait.jpg"
+import AdCarousel from "../components/AdCarousel";
 
 export default function HomePage() {
     usePreloadImage([starryBG, logo, ifYoureLooking])
@@ -35,7 +36,7 @@ export default function HomePage() {
                             <div className="d-flex flex-column justify-content-evenly align-items-center p-3">
                                 {/* <div className="d-inline-flex border border-3 rounded border-secondary bg-danger text-light shadow-2b m-3 flex-column">
                                     <h3 className="h1 p-3"><strong>Our opening weekend of the 2025 Season will be <br />4/11/2025 & 4/12/2025!</strong></h3>
-                                </div> */}
+                                    </div> */}
 
                                 <div>
                                     <h4 className="h2 pt-3 pb-1">Please remember that we are a <strong><u>cash only</u></strong> business!</h4>
@@ -101,6 +102,7 @@ export default function HomePage() {
                     </div>
                 </section>
             </div>
+            <AdCarousel></AdCarousel>
             <section>
                 <div id="google-map-embed" className="d-flex justify-content-center align-items-center mx-3 mt-3 mw-100">
                     <iframe
