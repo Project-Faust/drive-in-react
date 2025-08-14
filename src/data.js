22047a1118b98939860f69804f324cfa45cdc5bf
project-faust <pentecost.cody.lane@gmail.com>
2025-08-13 22:12:27 -0400

    updated for upcoming weekends 8/22 & 8/29

--- src/data.js
+++ src/data.js
@@ -37,13 +37,23 @@ export const toBeDetermined = [
 // eslint-disable-next-line
 export const comingSoon = [
   {
-    title: "To Be Determined",
-    image: require("./assets/to-be-determined.jpg"),
-    imdb: "",
+    title: "Weapons (2025)",
+    image: require("../src/assets/movie-posters/weapons-2025.webp"),
+    imdb: "https://www.imdb.com/title/tt26581740/",
   },
   {
-    title: "To Be Determined",
-    image: require("../src/assets/to-be-determined.jpg"),
-    imdb: "",
+    title: "It's Alive (1974)",
+    image: require("../src/assets/movie-posters/its-alive-1974.webp"),
+    imdb: "https://www.imdb.com/title/tt0071675/",
+  },
+  {
+    title: "The Bad Guys 2 (2025)",
+    image: require("../src/assets/movie-posters/bad-guys-2-2025.webp"),
+    imdb: "https://www.imdb.com/title/tt30017619/"
+  },
+  {
+    title: "Jaws (1975)",
+    image: require("../src/assets/movie-posters/jaws-1975.webp"),
+    imdb: "https://www.imdb.com/title/tt0073195/"
   }
 ]
--- src/pages/ComingSoon.js
+++ src/pages/ComingSoon.js
@@ -2,101 +2,105 @@
 import { comingSoon, nowPlaying, toBeDetermined } from "../data";
 
 const posterStyle = {
-    maxWidth: "95%",
-    maxHeight: "800px",
-    aspectRatio: "2/3"
+  maxWidth: "100%",
+  maxHeight: "800px",
+  aspectRatio: "2/3"
 };
 
 export default function ComingSoon() {
-    //eslint-disable-next-line
-    const currentWeekend = [
-        {
-            dates: { start: "6/6/2025", end: "6/7/2025" },
-            movies: [comingSoon[0], comingSoon[1]]
-        },
-    ]
+  //eslint-disable-next-line
+  const currentWeekend = [
+    {
+      dates: { start: "6/6/2025", end: "6/7/2025" },
+      movies: [comingSoon[0], comingSoon[1]]
+    },
+  ]
 
 
-    const upcomingWeekends = [
+  const upcomingWeekends = [
 
-        {
-            dates: { start: "8/22/2025", end: "8/23/2025" },
-            movies: [toBeDetermined[1], toBeDetermined[0]]
-        },
-    ];
+    {
+      dates: { start: "8/22/2025", end: "8/23/2025" },
+      movies: [comingSoon[0], comingSoon[1]]
+    },
+    {
+      dates: {start: "8/29/2025", end: "8/30/2025" },
+      movies: [comingSoon[2], comingSoon[3]]
+    },
+  ];
 
-    return (
-        <section id="currentFeaturesParent" className="bg-secondary min-vh100 mb-0">
-            <div className="text-center">
-                {/* Header Card */}
-                <div className="d-inline-flex text-center border border-3 border-primary rounded text-light shadow-lg lg:m-5 m-2 mb-0">
-                    <div className="" style={{ backgroundColor: "#000000" }}>
-                        <h1><strong>Coming Soon!</strong></h1>
-                        <p>Click or tap the movie poster below to see the IMDb entry!</p>
-                        <p>If there is no date posted, our booking has not yet been confirmed for that date.</p>
-                        <p>If there is a "To Be Determined" placeholder, our booking has not yet been confirmed for that movie.</p>
-                    </div>
+  return (
+    <section id="currentFeaturesParent" className="min-vh100 m-0">
+      <div className="text-center">
+        {/* Header Card */}
+        <div className="d-inline-flex text-center border border-3 border-primary rounded text-light shadow-lg lg:m-5 m-3 mb-0">
+          <div className="" style={{ backgroundColor: "#000000" }}>
+            <h1><strong>Coming Soon!</strong></h1>
+            <p>Click or tap the movie poster below to see the IMDb entry!</p>
+            <p>If there is no date posted, our booking has not yet been confirmed for that date.</p>
+            <p>If there is a "To Be Determined" placeholder, our booking has not yet been confirmed for that movie.</p>
+          </div>
+        </div>
+
+        {/* Movies Sections - One for each weekend */}
+        <div className="text-light">
+          {upcomingWeekends.map((weekend, weekendIndex) => (
+            <section
+              key={`weekend-${weekendIndex}`}
+              className="row text-center bg-dark border-top rounded m-5"
+            >
+              {/* Date Header */}
+              <div className="d-inline-flex justify-content-center m-5 mx-auto">
+                <div className="text-center border border-3 border-primary rounded shadow-lg p-3" style={{ backgroundColor: "#000000" }}>
+                  <h2>Weekend of <strong>{weekend.dates.start}</strong> & <strong>{weekend.dates.end}</strong>!</h2>
+                  <h3 className="my-3">
+                    <strong>{weekend.movies[0].title}</strong> will be followed by <strong>{weekend.movies[1].title}</strong>!
+                  </h3>
                 </div>
+              </div>
 
-                {/* Movies Sections - One for each weekend */}
-                <div className="text-light">
-                    {upcomingWeekends.map((weekend, weekendIndex) => (
-                        <section
-                            key={`weekend-${weekendIndex}`}
-                            className="row text-center bg-dark rounded m-5"
+              {/* Movie Cards - Only map through the 2 movies for this weekend */}
+              <div className="">
+                <section className="row justify-content-evenly text-center">
+                  {weekend.movies.map((movie, index) => (
+                    <div key={`weekend-${weekendIndex}-movie-${index}`} className="col-lg-5 col-md-12">
+                      <div
+                        className="border border-danger border-3 rounded shadow-lg p-3 my-3"
+                        style={{ backgroundColor: "#000000" }}
+                      >
+                        <a
+                          href={movie.imdb}
+                          target="_blank"
+                          rel="noreferrer"
+                          className="text-decoration-none"
                         >
-                            {/* Date Header */}
-                            <div className="d-inline-flex justify-content-center m-3 mx-auto">
-                                <div className="text-center border border-3 border-primary rounded shadow-lg p-3" style={{ backgroundColor: "#000000" }}>
-                                    <h2>Weekend of <strong>{weekend.dates.start}</strong> & <strong>{weekend.dates.end}</strong>!</h2>
-                                    <h3 className="my-3">
-                                        <strong>{weekend.movies[0].title}</strong> will be followed by <strong>{weekend.movies[1].title}</strong>!
-                                    </h3>
-                                </div>
-                            </div>
+                          <h1 className="text-light pt-2"><strong>{movie.title}</strong></h1>
+                        </a>
+                        <div className="container">
+                          <a
+                            href={movie.imdb}
+                            target="_blank"
+                            rel="noreferrer"
+                          >
+                            <img
+                              src={movie.image}
+                              alt={`${movie.title} poster`}
+                              className="border border-light rounded shadow-lg"
+                              style={posterStyle}
+                            />
+                            <p className="text-light">Image source: IMDb</p>
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </section>
+              </div>
+            </section>
+          ))}
 
-                            {/* Movie Cards - Only map through the 2 movies for this weekend */}
-                            <div className="my-3">
-                                <section className="row justify-content-evenly text-center">
-                                    {weekend.movies.map((movie, index) => (
-                                        <div key={`weekend-${weekendIndex}-movie-${index}`} className="col-lg-5 col-md-12">
-                                            <div
-                                                className="border border-danger border-3 rounded shadow-lg p-3 my-3"
-                                                style={{ backgroundColor: "#000000" }}
-                                            >
-                                                <a
-                                                    href={movie.imdb}
-                                                    target="_blank"
-                                                    rel="noreferrer"
-                                                    className="text-decoration-none"
-                                                >
-                                                    <h1 className="text-light pt-2"><strong>{movie.title}</strong></h1>
-                                                </a>
-                                                <div className="container">
-                                                    <a
-                                                        href={movie.imdb}
-                                                        target="_blank"
-                                                        rel="noreferrer"
-                                                    >
-                                                        <img
-                                                            src={movie.image}
-                                                            alt={`${movie.title} poster`}
-                                                            className="border border-light rounded shadow-lg"
-                                                            style={posterStyle}
-                                                        />
-                                                        <p className="text-light">Image source: IMDb</p>
-                                                    </a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    ))}
-                                </section>
-                            </div>
-                        </section>
-                    ))}
-
-                    {/* TBD Weekend Section */}
-                    {/* <section className="row text-center bg-dark rounded mx-5">
+          {/* TBD Weekend Section */}
+          {/* <section className="row text-center bg-dark rounded mx-5">
                         <div className="d-inline-flex justify-content-center my-4 mx-auto">
                             <div className="text-center border border-3 border-primary rounded p-3" style={{ backgroundColor: "#000000" }}>
                                 <h2>Weekend of <strong>6/27/2025</strong> & <strong>6/28/2025</strong>!</h2>
@@ -130,8 +134,8 @@ export default function ComingSoon() {
                             </section>
                         </div>
                     </section> */}
-                </div>
-            </div>
-        </section>
-    );
+        </div>
+      </div>
+    </section>
+  );
 }
