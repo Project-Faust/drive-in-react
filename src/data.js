bc5121bd20e0ea2b94ac38cbcda7815b339e817e
project-faust <pentecost.cody.lane@gmail.com>
2025-11-03 11:03:04 -0500

    updated with harry potter weekend titles/images

--- src/data.js
+++ src/data.js
@@ -9,14 +9,24 @@ const template = [
 
 export const nowPlaying = [
   {
-    title: "Halloween (1978)",
-    image: "https://m.media-amazon.com/images/M/MV5BMzZiNTdiYTgtYjNkMS00MmJmLWEwZGQtNmY0NGJkMGE0YmYzXkEyXkFqcGc@._V1_.jpg",
-    imdb: "https://www.imdb.com/title/tt0077651/",
+    title: "Harry Potter and the Order of the Phoenix (2007)",
+    image: "https://m.media-amazon.com/images/M/MV5BYWJmM2M1YzItMjY1Ni00YzRmLTg5YWYtNDFmNTJjNzQ0ODkyXkEyXkFqcGc@._V1_.jpg",
+    imdb: "https://www.imdb.com/title//",
   },
   {
-    title: "Nightmare on Elm Street (1984)",
-    image: "https://m.media-amazon.com/images/M/MV5BNTZmN2VlMWMtMTdlMi00NDBhLWIyODMtODEwMzM0ODMwMWYwXkEyXkFqcGc@._V1_.jpg",
-    imdb: "https://www.imdb.com/title/tt0087800/"
+    title: "Harry Potter and the Half-Blood Prince (2009)",
+    image: "https://m.media-amazon.com/images/M/MV5BNzU3NDg4NTAyNV5BMl5BanBnXkFtZTcwOTg2ODg1Mg@@._V1_.jpg",
+    imdb: "https://www.imdb.com/title/tt0417741/"
+  },
+  {
+    title: "Harry Potter and the Deathly Hallows: Part 1 (2010)",
+    image: "https://m.media-amazon.com/images/M/MV5BMTQ2OTE1Mjk0N15BMl5BanBnXkFtZTcwODE3MDAwNA@@._V1_.jpg",
+    imdb: "https://www.imdb.com/title/tt0373889/"
+  },
+  {
+    title: "Harry Potter and the Deathly Hallows: Part 2 (2011)",
+    image: "https://m.media-amazon.com/images/M/MV5BOTA1Mzc2N2ItZWRiNS00MjQzLTlmZDQtMjU0NmY1YWRkMGQ4XkEyXkFqcGc@._V1_.jpg",
+    imdb: "https://www.imdb.com/title/tt1201607/"
   }
 ]
 
