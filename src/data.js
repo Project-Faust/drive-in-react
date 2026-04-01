4dd69c5a823ad703171f00f871afbe1db8738b62
project-faust <pentecost.cody.lane@gmail.com>
2026-04-01 17:43:31 -0400

    added for opening weekend 4/10/2026

--- src/data.js
+++ src/data.js
@@ -7,16 +7,15 @@ const template = [
   // }
 ]
 
-export const nowPlaying = [
-  {
-    title: "Elf (2003)",
-    image: "https://m.media-amazon.com/images/M/MV5BNDQ0ZWE2NzgtNGNhMC00MDIwLWI1MjUtYjYxZGRiM2UyYTQzXkEyXkFqcGc@._V1_.jpg",
-    imdb: "https://www.imdb.com/title/tt0319343/"
+export const nowPlaying = [  {
+    title: "To Be Determined",
+    image: require("./assets/to-be-determined.jpg"),
+    imdb: ""
   },
   {
-    title: "National Lampoon's Christmas Vacation (1989)",
-    image: "https://m.media-amazon.com/images/M/MV5BZDgxYzI2YWItNmUyNS00ZWE4LWEzZDctYTk1M2VkYjhkOWVmXkEyXkFqcGc@._V1_.jpg",
-    imdb: "https://www.imdb.com/title/tt0097958/"
+    title: "To Be Determined",
+    image: require("../src/assets/to-be-determined.jpg"),
+    imdb: ""
   }
 ]
 
@@ -36,4 +35,16 @@ export const toBeDetermined = [
 
 // eslint-disable-next-line
 export const comingSoon = [
+
+  {
+    title: "The Super Mario Galaxy Movie (2026)",
+    image: "https://m.media-amazon.com/images/M/MV5BMDg5MjRkNWEtYmU1Mi00MTExLTk5MDQtY2RiMWVkZWNiOThjXkEyXkFqcGc@._V1_.jpg",
+    imdb: "https://www.imdb.com/title/tt28650488/"
+  },
+  {
+    title: "Project Hail Mary (2026)",
+    image: "https://m.media-amazon.com/images/M/MV5BNTkwNzJiYTctNzI3NC00NjE1LTlhYjktY2Q5MTdmMWFmNzcxXkEyXkFqcGc@._V1_.jpg",
+    imdb: "https://www.imdb.com/title/tt12042730/"
+  }
 ]
+
