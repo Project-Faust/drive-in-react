2926f2fca8e3a7d55b6c6fca61d613a1f5efdc66
Project-Faust <pentecost.cody.lane@gmail.com>
2026-05-24 19:02:47 -0400

    new ad images; updated weekend 5/29

--- src/data.js
+++ src/data.js
@@ -9,16 +9,16 @@ const template = [
 
 export const nowPlaying = [
   {
+    title: "The Sheep Detectives",
+    image:
+      "https://m.media-amazon.com/images/M/MV5BNTFmZWI4YmMtNmQ0ZC00ZGQwLTk1OWEtZjAyZmIzOGY0MGFiXkEyXkFqcGc@._V1_.jpg",
+    imdb: "https://www.imdb.com/title/tt32565993/",
+  },
+  {
     title: "Star Wars: The Mandalorian & Grogu",
     image:
       "https://m.media-amazon.com/images/M/MV5BYzVkMmJhNTgtNjYwOS00YjM0LThlNWEtNGExNmIxZjVkMmJhXkEyXkFqcGc@._V1_.jpg",
     imdb: "https://www.imdb.com/title/tt30825738/",
-  },
-  {
-    title: "Send Help",
-    image:
-      "https://m.media-amazon.com/images/M/MV5BOTgxOWY5NTMtYjdiNi00NTIyLTkwMTQtMWNjM2IzYTU0ODgyXkEyXkFqcGc@._V1_.jpg",
-    imdb: "https://www.imdb.com/title/tt8036976/",
   },
 ];
 
