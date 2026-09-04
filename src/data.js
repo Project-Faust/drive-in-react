08de8754d87c0227b338f77ad7a470fa7a4c9e63
project-faust <pentecost.cody.lane@gmail.com>
2026-09-04 13:48:25 -0400

    updated end of oak street 9/11

--- src/data.js
+++ src/data.js
@@ -36,13 +36,13 @@ export const toBeDetermined = [
 // eslint-disable-next-line
 export const comingSoon = [
   {
-    title: "To Be Determined",
-    image: require("../src/assets/to-be-determined.jpg"),
-    imdb: "",
+    title: "Practical Magic 2",
+    image: "https://m.media-amazon.com/images/M/MV5BMWUyY2UyNjgtYTg5MC00MDZmLWE1MzEtZmU2YWE2ZTMzMjUwXkEyXkFqcGc@._V1_.jpg",
+    imdb: "https://www.imdb.com/title/tt32588798/",
   },
   {
-    title: "To Be Determined",
-    image: require("../src/assets/to-be-determined.jpg"),
-    imdb: "",
+    title: "The End of Oak Street",
+    image: "https://m.media-amazon.com/images/M/MV5BZGI0YmRjNWYtYTliYy00YzdjLTkzNzQtZWExNjA2ZDU5MzAwXkEyXkFqcGc@._V1_.jpg",
+    imdb: "https://www.imdb.com/title/tt27165187/",
   },
 ];
